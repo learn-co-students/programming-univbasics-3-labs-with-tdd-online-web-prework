3ab857752fcd1a5c7c5d4a854220856edf21e2b9
Patrick Harrison <jahman@bellatlantic.net>
2019-06-13 23:33:01 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,6 @@
-# Add your variables here
+first_number =6 # Add your variables here
+second_numer =2
+sum = "first_number"+"second_number"
+sum = "first_number"-"second_number"
+sum = "first_number"/"second_number"
+sum = "first_number"*"second_number"
