6fc7ce4899bde388fc8c5779dc3b5ace45358e2b
LaNice Powell <lanicemp@aol.com>
2019-06-27 11:25:46 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
-'require_relative'../calculator.rb'
-
-# Add your variables here 
-
-first_number = "1.0" 
-second_number = "2.0"
+first_number = 1.0 
+second_number = 2.0
+sum = (first_number + second_number)
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
