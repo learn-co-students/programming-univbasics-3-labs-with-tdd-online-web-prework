ef00b2354a2fe42e55086d90dd53d2b9e01476bf
cheerful-transform-7743 <daniel.siekawitch@gmail.com>
2019-07-05 17:15:22 +0000

    Done.

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,3 +1,4 @@
+
 # Code your solution in calculator.rb
 
 describe "./calculator.rb" do
@@ -20,7 +21,7 @@ describe "./calculator.rb" do
     second_number = get_variable_from_file('./calculator.rb', "second_number")
     sum = get_variable_from_file('./calculator.rb', "sum")
 
-    expect(sum).to eq(first_number+second_number)
+    expect(sum).to equal(first_number+second_number)
   end
 
   it "contains a local variable called difference that is assigned to the result of subtracting first_number and second_number" do
@@ -28,7 +29,7 @@ describe "./calculator.rb" do
     second_number = get_variable_from_file('./calculator.rb', "second_number")
     difference = get_variable_from_file('./calculator.rb', "difference")
 
-    expect(difference).to eq(first_number-second_number)
+    expect(difference).to equal(first_number-second_number)
   end
 
   it "contains a local variable called product that is assigned to the result of multiplying first_number and second_number" do
@@ -36,7 +37,7 @@ describe "./calculator.rb" do
     second_number = get_variable_from_file('./calculator.rb', "second_number")
     product = get_variable_from_file('./calculator.rb', "product")
 
-    expect(product).to eq(first_number*second_number)
+    expect(product).to equal(first_number*second_number)
   end
 
   it "contains a local variable called quotient that is assigned to the result of dividing first_number by second_number" do
@@ -44,6 +45,6 @@ describe "./calculator.rb" do
     second_number = get_variable_from_file('./calculator.rb', "second_number")
     quotient = get_variable_from_file('./calculator.rb', "quotient")
 
-    expect(quotient).to eq(first_number/second_number)
+    expect(quotient).to equal(first_number/second_number)
   end
 end
