6fab54e1eb81839245cd0794ef2a394696f75bcc
Maria Guillian A. Edulsa <edulsag03@gmail.com>
2019-07-09 22:49:45 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,4 @@
-# Add your variables here
+expect(first_number).to be_an(50)
+expect(second_number).to be_an (10)
+expect(second_number).not_to equal (0)
+expect(sum).to eq(first_number+second_number)
