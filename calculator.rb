ebd285201302a04f382f51df0648810eb1101bcd
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-07-21 19:14:18 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,6 @@
-# Add your variables here
+first_number = 28
+second_number = 2
+sum = second_number + first_number 
+difference = first_number - second_number
+product = first_number * second_number 
+quotient = first_number / second_number
