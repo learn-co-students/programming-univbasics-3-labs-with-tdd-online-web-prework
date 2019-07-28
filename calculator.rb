525defd9e5284c96a6b444e2650f2114c1bc59d7
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-07-28 19:11:52 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,7 @@
 # Add your variables here
+Integer first_number = 12
+Float second_number = 2.1
+Float sum = first_number + second_number
+Float difference = first_number - second_number
+Float product = first_number * second_number
+Float quotient = first_number / second_number
