37f51f24fc690313543d5d9b0cc3d8ab36783914
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-07-31 08:36:13 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,13 @@
-# Add your variables here
+def first_number = 1
+end
+def second_number = 3
+end
+  if sum
+    puts first_number + second_number
+  elsif difference
+    puts first_number - second_number
+  elsif product
+    puts first_number * second_number
+  else quotient
+    puts first_number / second_number
+  end
