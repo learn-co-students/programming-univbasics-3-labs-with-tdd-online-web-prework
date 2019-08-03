36a5517271e186b9269c3f91eaeca152fc0b5874
attentive-rubyist-8517 <robby.medina562@gmail.com>
2019-08-03 02:52:37 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,6 @@
-# Add your variables here
+First_number=n
+second_number !=0 second_number=n
+sum= First_number+second_number
+difference= First_number - second_number
+product=First_number * second_number
+quotient=first_number / second_number
