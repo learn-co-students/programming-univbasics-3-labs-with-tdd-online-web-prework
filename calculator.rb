0af59d34bb23017b375fa87273115df5d2084e49
Lauren Dove <elledoveinc@gmail.com>
2019-08-11 01:10:51 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,13 @@
 first_number = 10
+
 second_number = 25
+
 sum = first_number + second_number
-difference = second_number - first_number
-product first_number * second_number
-quotient = second_number / first_number# Add your variables here 
+
+difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
+
+# Add your variables here 
