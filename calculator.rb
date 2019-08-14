1f4e7e8cd6035429af0dc325a5d0e78bdd866236
John Reyes <john.jreye@gmail.com>
2019-08-14 03:24:50 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 
-expect_first_number("5")
-second_number("3")
-sum = "first_number" + "second_number"
+first_number = 5
+second_number  = 3
+sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
 quotient = first_number / second_number
