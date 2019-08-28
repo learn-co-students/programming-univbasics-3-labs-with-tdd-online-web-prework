df877cc9b480598c86059c1cdbe81dcbf70896e5
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-08-28 23:36:23 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,6 @@
 first_number = Integer or Float
 second_number = Integer or Float
+expect(second_number).not_to equal(0)
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
