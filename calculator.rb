2433839892496da4113b0296b01a218fe20805ff
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-08-28 23:25:40 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,5 @@
-# Add your variables here
+first_number = 5
+second_number = 6
+expect(second_number).not_to equal(0)
+difference = first_number - second_number
+quotient = first_number / second_number
