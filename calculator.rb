2da6874e4205fe86c95cbf14c0c075b69b511dad
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-08-28 23:33:43 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,6 @@
 first_number = 5
 second_number = 6
-expect(second_number).not_to equal(0)
+sum = first_number + second_number
 difference = first_number - second_number
+product = first_number * second_number
 quotient = first_number / second_number
