726aa35aab6d36ee3400fb89f9226a83008ed16e
Duong Dang <dddangmedia@gmail.com>
2019-09-03 22:21:08 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,22 @@
-# Add your variables here
+first_number = 2 
+second_number = 1
+
+def sum(first_number, second_number)
+  sum = first_number + second_number
+  puts sum  
+end
+
+def difference(first_number, second_number)
+  difference = first_number - second_number
+  puts difference
+end
+
+def product(first_number, second_number)
+  product = first_number * second_number
+  puts product
+end
+
+def quotient(first_number, second_number)
+  quotient = first_number / second_number
+  print quotient
+end
