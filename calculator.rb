004700e2fe037a786101f5ad8fc5b227cf974b45
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-03 15:23:56 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,7 @@
-# Add your variables here
+first_number = 2
+second_number = 6
+sum = ( first_number + second_number )
+difference =  ( second_number - first_number )
+product = ( first_number * second_number )
+quotient = ( second_number / first_number )
+learn submit
