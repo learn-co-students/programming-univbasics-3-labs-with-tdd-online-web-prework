b3e92978420c9e4977314934728f1cbe7684878e
global-function-2971 <lastryko327@gmail.com>
2019-09-27 02:30:13 +0000

    Done.

---
+++ Failcalculator.rb
@@ -0,0 +1,18 @@
+def first_number(fn)
+  puts give me a number.
+fn = gets.strip 
+end
+def second_number(sn)
+ puts give me another number.
+sn = gets.strip
+end
+def operration(op)
+  puts What operration ( + , - , / , * , ** )
+op = gets.strip
+
+first_number.to = (fn)
+second_number.to = (sn)
+
+(first_number (op) second_number) = (answer) 
+
+puts "Equals (answer)"
