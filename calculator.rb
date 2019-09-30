35e2804a4557e10385316c5a64002027700dacd9
RokhayaN <rokhayamuslima1@gmail.com>
2019-09-30 17:24:37 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,15 @@
-# Add your variables here
+first_number = 1 # Add your variables here
+def first_number('./calculator.rb',"1")
+  puts 'first_number' 
+end 
+second_number =! 0 
+  puts 'first_number' += 1 
+end 
+
+
+
+
+
+
+
+
