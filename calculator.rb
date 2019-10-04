c11743db4c0d2386560b0e0ce7d178e0b266ffd1
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-10-04 11:00:14 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,13 @@
 # Add your variables here
+first_number = 15
+second_number = 3
+add_number = first_number + second_number
+mult_number = first_number * second_number
+subs_number = first_number - second_number
+quotient = first_number / second_number
+sum_number = add_number + subs_number
+puts add_number
+puts mult_number
+puts subs_number
+puts quotient
+puts sum_number
