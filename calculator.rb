dc121dbfa56c8cdd142758d0eafe6a06cdd59ff2
Christian Jimenez <christianjimenez0123@gmail.com>
2019-10-11 19:37:48 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,7 @@
-first_number = 4
+first_number = 0
 second_number = 3
 sum = first_number + second_number
-puts sum
+
+difference = first_number-second_number
+product = first_number*second_number
+quotient = first_number/second_number
