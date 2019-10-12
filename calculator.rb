e34f2459985c74d78371da76578159ffaa8b0d78
Melissa Holt <melissa.holt21@gmail.com>
2019-10-12 07:12:19 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,15 @@
-# Add your variables here
+first_number = 14
+second_number = 7
+
+def sum = (first_number + second_number)
+  puts "The sum is #{sum}"
+end
+def difference = (first_number - second_number)
+  puts "The difference is #{difference}."
+end
+def product = (first_number * second_number)
+  puts "The product is #{product}."
+end
+def quotient= (first_number / second_number)
+  puts "The quotient is #{quotient}."
+end
