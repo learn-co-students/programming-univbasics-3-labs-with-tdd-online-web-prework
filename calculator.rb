daa4fddcec23b7e0e99c8dbbceb4c5f9e39e8cb8
Sara Antoine <blythejolei@gmail.com>
2019-10-15 21:58:47 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,8 @@
-# Add your variables here
+0=0
+first_number = 0
+1=1
+second_number = 1
+sum=first_number+second_number
+difference=second_number-first_number
+product=first_number*second_number
+quotient=first_number/second_number
