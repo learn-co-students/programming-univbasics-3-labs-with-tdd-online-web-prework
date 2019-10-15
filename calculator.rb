dfa96e9838b156175d3e6c3ef35769e71eb15a11
Kevin Thompson <indelible11@yahoo.com>
2019-10-15 13:07:40 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,6 @@
-# Add your variables here
+first_number = 2
+second_number = 3
+third_number = first_number+second_number
+fourth_number = first_number-second_number
+fifth_number = first_number*second_number
+sixth_number = first_number/second_number
