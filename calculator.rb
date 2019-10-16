01dab93e01326a6400977b294e7bfc98e2a6b6a6
Ayako Wiltshire <awittyl44@gmail.com>
2019-10-16 16:30:23 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,12 @@
 # Add your variables here
+def first_number = 9.0
+  
+def second_number = 6.0
+
+def sum = first_number + second_number 
+  
+def difference = first_number - second_number
+  
+def product = first_number * second_number
+  
+def quotient = first_number / second_number
