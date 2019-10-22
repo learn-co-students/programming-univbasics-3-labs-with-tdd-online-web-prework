fbe2e241b7ea21da4cab0749266dece741bb3860
qiong sun <joannesun44@gmail.com>
2019-10-22 19:16:55 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,13 @@
-# Add your variables here
+def calculator
+first_number = #{X, "Integer" || "Float"}
+second_number = #{Y, "Integer" || "Float", Y!= 0}
+    
+    
+end
+
+Sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
+
+end 
