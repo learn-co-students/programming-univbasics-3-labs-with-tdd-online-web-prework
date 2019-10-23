11c7baa45f39f66bd344687c234c28064798f64d
Taishawn King <taishawnking1@yahoo.com>
2019-10-23 22:34:17 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,18 @@
-# Add your variables here
+first_number = 1 
+second_number = 1 
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
+
+
+
+
+
+
+#- The third test is looking for a local variable named `sum`. The `sum` variable
+  #is the result of adding `first_number` and `second_number` together. This test
+ # is using all three variables. Not only that, the test is using whatever values
+  #_you_ assigned to `first_number` and `second_number`.
+
+#`quotient` for dividing the `first_number` and `second_number` variables.
