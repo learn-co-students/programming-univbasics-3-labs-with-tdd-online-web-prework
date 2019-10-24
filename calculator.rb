0a874148c95002abdc210a712dea4a2fea9d5231
Matthew Gellert <kifireguy@gmail.com>
2019-10-24 21:10:47 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,11 @@
-
+# File: spec/calculator_spec.rb
+require './calculator.rb'
+describe "calculator" do
+it 'adds numbers' do
+    ...
+  end
+it 'subtracts numbers' do
+    calc = Calculator.new
+    expect(calc.subract(5,1)).to eql(4)
+  end
+end
