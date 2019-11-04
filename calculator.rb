fa70594cf55903ab46b942c07d5268ce205f35e4
Almmond Hudnall <ahudnall24@gmail.com>
2019-11-04 20:06:08 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,13 @@
-# Add your variables here
+./calculator.rb
+expect(first_number).equal_to(x)
+  x=2
+expect(second_number).not_to equal(y)
+  y=3
+expect(sum).x+y(0)
+expect(sum).5-y(x)
+expect(sum).x*1(x)
+expect(sum).6/x(y)
+
+
+
+
