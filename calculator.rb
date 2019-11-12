fc489d2475127e1db4652a75873ca846513e18f7
Brendan R Borgers <brendanborgers@gmail.com>
2019-11-12 20:11:27 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,31 +1,11 @@
-do
-first_number = get_variable_from_file('./calculator.rb', 'first_number')
-expect(first_number).to be_an(Integer) .or be_an(Float)
-end
+first_number = 10
 
-do
-second_number = get_variable_from_file('./calculator.rb', 'second_number')
-expect(second_number).to be_an(Integer) .or be_an(Float)
-expect(second_number) .not_to equal(0)
-end
+second_number = 10
 
-do
-sum = get_variable_from_file('./calculator.rb', 'sum')
- expect(sum).to eq(first_number + second_number)
- end
- 
- do
-   difference = get_variable_from_file('./calculator.rb', "difference")
-   expect(difference).to eq(first_number - second_number)
-end
+sum = 20
 
-do
-  product = get_variable_from_file('./calculator.rb', "product")
-  expect(product).to eq(first_number * second_number)
-end
+difference = 0
 
-do
-  quotient = get_variable_from_file('./calculator.rb', "quotient")
-  expect(quotient).to eq(first_number / second_number)
-end
-end
+product = 100
+
+quotient = 1
