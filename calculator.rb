a0e46467d785ccc13d9837acb1c7ef25e11e320a
Devon James Wright <devonjamess.wright@gmail.com>
2019-11-18 13:50:10 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,9 @@
-# Add your variables here
+
+first_number = 4
+second_number = 5
+sum = first_number + second_number
+third_number = 12
+fourth_number = 45
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number/second_number
