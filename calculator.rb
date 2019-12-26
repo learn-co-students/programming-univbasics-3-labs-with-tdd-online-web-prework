938e11c83ebfd663fec6153461c8816be866e3ce
fadimata tillman <fadi.tillman@gmail.com>
2019-12-26 01:37:39 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,20 @@
-# Add your variables here
+# Add your variables
+describe "./calculator.rb" 
+first_number = get_variable_from_file('./calculator.rb', "first_number")
+
+    expect(first_number).to be_an(Integer).or be_a(Float)
+    expect(second_number).not_to equal(0)
+  end
+  first_number = get_variable_from_file('./calculator.rb', "first_number")
+    second_number = get_variable_from_file('./calculator.rb', "second_number")
+    sum = get_econd_number = get_varfirst_number = get_variable_from_file('./calculator.rb', "first_number")
+    second_number = get_variable_from_file('./calculator.rb', "second_number")
+    difference = get_variable_from_file('./calculator.rb', "difference")
+
+    expect(difference).to eq(first_number-second_number)
+  end
+   first_number = get_variable_from_file('./calculator.rb', "first_number")
+    second_number = get_variable_from_file('./calculator.rb', "second_number")
+    product = get_variable_from_file('./calculator.rb', "product")umber/second_number)
+  end
+end
