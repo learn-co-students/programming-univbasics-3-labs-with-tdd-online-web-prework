b23898643d268d405607cf7006641e7ae4ac91e4
Karen Daniela <kdaniimora@gmail.com>
2020-01-11 21:20:19 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,16 @@
 # Add your variables here
+
+first_number= "Hello world"
+second_number= not_to equal(0)
+def
+sum(first_number + second_number)
+end
+def
+difference (first_number - second_number)
+end
+def
+product (first_number * second_number)
+end
+def
+quotient (first_number / second_number)
+end
